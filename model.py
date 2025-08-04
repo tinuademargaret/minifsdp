0b83feee26a010fb73f747982b20bae99c008d15
tinuademargaret <tinuademargaret@gmail.com>
2025-08-04 15:45:18 +0100

    add gpu process initialization

--- model.py
+++ model.py
@@ -5,26 +5,38 @@ import torch.multiprocessing as mp
 
 
 def run(rank, world_size):
-    print(f"I am rank {rank} of {world_size}")
+    print(f"Hello from rank {rank} of {world_size}")
+    assert rank == dist.get_rank()
+    assert world_size == dist.get_world_size()
 
 
-def init_process(rank, world_size, fn, backend="gloo"):
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "29500"
-    dist.init_process_group(backend, rank=rank, world_size=world_size)
-    fn(rank, world_size)
+def init_process(rank=None, world_size=None, fn=None, backend="gloo", cuda=False):
+    if cuda:
+        dist.init_process_group(backend=backend)
+        rank = int(os.environ["RANK"])
+        world_size = int(os.environ["WORLD_SIZE"])
+        fn(rank, world_size)
+    else:
+        os.environ["MASTER_ADDR"] = "localhost"
+        os.environ["MASTER_PORT"] = "29500"
+        dist.init_process_group(backend, rank=rank, world_size=world_size)
+        fn(rank, world_size)
 
 
 if __name__ == "__main__":
-    world_size = 2
-    processes = []
+    cuda = torch.cuda.is_available()
+    if cuda:
+        init_process(fn=run, backend="nccl", cuda=True)
+    else:
+        world_size = 2
+        processes = []
 
-    mp.set_start_method("spawn")
+        mp.set_start_method("spawn")
 
-    for rank in range(world_size):
-        process = mp.Process(target=init_process, args=(rank, world_size, run))
-        process.start()
-        processes.append(process)
+        for rank in range(world_size):
+            process = mp.Process(target=init_process, args=(rank, world_size, run))
+            process.start()
+            processes.append(process)
 
-    for p in processes:
-        p.join()
+        for p in processes:
+            p.join()
